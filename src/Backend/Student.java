50c859e55806acfcaba0d37df6192ac0204d0bd3
Ahmed Essam <ahmedessam@Abdullahs-MacBook-Air.local>
2025-11-24 18:26:46 +0200

    Added certification panel and view certificate button for students

--- src/Backend/Student.java
+++ src/Backend/Student.java
@@ -6,23 +6,43 @@ package Backend;
 
 import java.util.ArrayList;
 
+// completed courses
+// add certificate
+
 
 public class Student extends User{
 
    private ArrayList <CourseProgress> enrolledCourses;
+   private ArrayList <Certificate> Certificates;
+   private ArrayList <String> CompletedCoursesIds;
 
 
-    public Student(  String username, String email, String passwordHash,ArrayList <CourseProgress> enrolledCourses) {
+    public Student(  String username, String email, String passwordHash,
+            ArrayList <CourseProgress> enrolledCourses,ArrayList <Certificate> Certificates,ArrayList <String> CompletedCoursesIds) {
         super(username,email,passwordHash,"student");
         this.role="Student";
        if(enrolledCourses==null)
         {this.enrolledCourses=new ArrayList<>();}
         else {this.enrolledCourses=enrolledCourses;}
+       if(Certificates==null)
+        {this.Certificates=new ArrayList<>();}
+        else {this.Certificates=Certificates;}
+       if(CompletedCoursesIds==null)
+        {this.CompletedCoursesIds=new ArrayList<>();}
+        else {this.CompletedCoursesIds=CompletedCoursesIds;}
         
     }
 
     public ArrayList<CourseProgress> getEnrolledCourses() {
         return enrolledCourses;
+       
+    }
+    public ArrayList<Certificate> getCertificates() {
+        return Certificates;
+       
+    }
+     public ArrayList<String> getCompletedCoursesIds() {
+        return CompletedCoursesIds;
        
     }
 
@@ -33,6 +53,9 @@ public class Student extends User{
         CourseProgress enrolledCourse= new CourseProgress(courseid);
         this.enrolledCourses.add(enrolledCourse);
         
+    }
+     public void addCertificate(Certificate certificate) {
+        Certificates.add(certificate);
     }
 
 }
